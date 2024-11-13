e208af5aafa398beb9ac3fd565a93da7c40d29e6
gabbi 🎀 <agabflix@gmail.com>
2024-11-13 11:27:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function responder(correta) {
+    if (correta) {
+        // Avançar para a próxima pergunta
+        
+       
+    } else {
+        // Entrar no modo tela cheia e exibir imagem de erro
+        setTimeout(() => {
+            goFullScreen();
+            exibirErro();
+        }, 2000);
+    }
+}
+
+
+
+// Função para exibir a imagem de erro e tocar o som
+function exibirErro() {
+    const telaCheiaErro = document.getElementById("telaCheiaErro");
+    const somErro = document.getElementById("somErro");
+
+    telaCheiaErro.style.display = "flex";  // Exibe a imagem em tela cheia
+    somErro.play();  // Toca o som de erro
+
+    // Clica na tela para sair do modo de erro
+    telaCheiaErro.addEventListener("click", () => {
+        telaCheiaErro.style.display = "none";
+        sairTelaCheia();
+    });
+}
+
+// Função para entrar no modo tela cheia
+function goFullScreen() {
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { // Firefox
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { // Chrome, Safari e Opera
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE/Edge
+        elem.msRequestFullscreen();
+    }
+}
+
+// Função para sair do modo tela cheia
+function sairTelaCheia() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { // Firefox
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { // Chrome, Safari e Opera
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { // IE/Edge
+        document.msExitFullscreen();
+    }
+}
